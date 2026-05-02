4635cb181a2e091d9d5479b240959560eca32588
Muneer S A <61188210+Muneer-S-A-05@users.noreply.github.com>
2026-05-03 03:48:08 +0530

    Remove: top-k-elements-in-list - submission-10

--- Data Structures & Algorithms/top-k-elements-in-list/submission-10.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        map = {}
-        for num in nums:
-            map[num] = map.get(num,0) + 1
-        return sorted(map,key=lambda x : map[x], reverse= True)[:k]
