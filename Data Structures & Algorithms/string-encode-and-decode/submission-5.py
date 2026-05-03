2b6322e71bd3215aa75850209188be262c711ffe
Muneer S A <61188210+Muneer-S-A-05@users.noreply.github.com>
2026-05-03 09:16:24 +0530

    Add: string-encode-and-decode - submission-5

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-5.py
@@ -0,0 +1,12 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        if strs:
+            return '-'.join(strs)
+        else:
+            return '#'
+    def decode(self, s: str) -> List[str]:
+        if s!='#':
+            return s.split('-')
+        else:
+            return []
