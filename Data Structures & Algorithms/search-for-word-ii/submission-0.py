2fbeb62ff507d6c04517ca7ade96ec34bac54d1c
Muneer S A <61188210+Muneer-S-A-05@users.noreply.github.com>
2026-06-28 03:31:02 +0530

    Add: search-for-word-ii - submission-0

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-0.py
@@ -0,0 +1,45 @@
+class Node:
+    def __init__(self):
+        self.children = {}
+        self.end = False
+
+    def addWord(self,word):
+        node = self
+        for x in word:
+            if x not in node.children:
+                node.children[x]=Node()
+            node = node.children[x]
+        node.end = True
+
+
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        root = Node()
+        for word in words:
+            root.addWord(word)
+        rows,cols = len(board),len(board[0])
+        res,visited = set(),set()
+
+        def dfs(r,c,word,node):
+            if not (0<=r<rows) or not(0<=c<cols) or (r,c) in visited or board[r][c] not in node.children:
+                return
+            
+            visited.add((r,c))
+
+            node = node.children[board[r][c]]
+            word += board[r][c]
+            if node.end:
+                res.add(word)
+            
+            dfs(r+1,c,word,node)
+            dfs(r-1,c,word,node)
+            dfs(r,c+1,word,node)
+            dfs(r,c-1,word,node)
+
+            visited.remove((r,c))
+
+        for r in range(rows):
+            for c in range(cols):
+                dfs(r,c,'',root)
+
+        return list(res)
