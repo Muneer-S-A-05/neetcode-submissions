06ab0fdf314efc4e2f0d72256ba3dfc834521ba5
Muneer S A <61188210+Muneer-S-A-05@users.noreply.github.com>
2026-08-10 19:04:47 +0530

    Add: jump-game - submission-4

---
+++ Data Structures & Algorithms/jump-game/submission-4.py
@@ -0,0 +1,14 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        if len(nums)==1:
+            return True
+
+        lastTrue = len(nums)-1
+        
+        for i in range(len(nums)-2,-1,-1):
+            if nums[i]+i >= len(nums):
+                lastTrue = i
+            elif lastTrue<=i+nums[i]:
+                lastTrue = i
+        
+        return lastTrue == 0
