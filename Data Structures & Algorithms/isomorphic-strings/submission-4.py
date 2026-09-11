626ed3d099ae09324721ae15259c81f4cfb2fb57
Muneer S A <61188210+Muneer-S-A-05@users.noreply.github.com>
2026-09-11 11:43:44 +0530

    Add: isomorphic-strings - submission-4

---
+++ Data Structures & Algorithms/isomorphic-strings/submission-4.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        if len(s)!=len(t): return False
+
+        smap = {}
+        tmap = {}
+
+        for i in range(len(s)):
+            if s[i] in smap and smap[s[i]] != t[i]:
+                return False
+            smap[s[i]] = t[i]
+
+            if t[i] in tmap and tmap[t[i]] != s[i]:
+                return False
+            tmap[t[i]] = s[i]
+        
+        return True
